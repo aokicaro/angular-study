3a4482edef5a711532e35ded1be5660429903418
aokicaro <aokicaro@hotmail.com>
2022-07-03 15:53:21 -0300

    string interpolation

--- data-binding-app/src/app/app.module.ts
+++ data-binding-app/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+
+import { AppComponent } from './app.component';
+import { StringInterpolationComponent } from './string-interpolation/string-interpolation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StringInterpolationComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ data-binding-app/src/app/string-interpolation/string-interpolation.component.ts
@@ -0,0 +1,13 @@
+import { Person } from './../shared/person.model';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-string-interpolation',
+  templateUrl: './string-interpolation.component.html',
+  styleUrls: ['./string-interpolation.component.css']
+})
+export class StringInterpolationComponent {
+
+person: Person;
+
+}
